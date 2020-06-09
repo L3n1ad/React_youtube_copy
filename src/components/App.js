e9dfcbc72ceb135a5e113d43aaf26e91660ae404
L3n1ad <danieltoth0910@gmail.com>
2020-06-09 16:17:20 +0100

    add videoList component and implement it in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../api/youtube";
+import VideoList from "./VideoList";
 
 const App = () => {
+  const [videos, setVideos] = useState([]);
+
+  async function onTermSubmit(term) {
+    const response = await youtube.get("/search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: process.env.REACT_APP_YOUTUBE_KEY,
+        q: term,
+      },
+    });
+    setVideos(response.data.items);
+  }
+
   return (
     <div className="ui container">
-      <SearchBar />
+      <SearchBar onTermSubmit={onTermSubmit} />
+      <VideoList videos={videos} />
     </div>
   );
 };
