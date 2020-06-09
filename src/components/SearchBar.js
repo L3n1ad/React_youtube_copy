9aa0375fd72e6078f99fe8cd024c5c38757f10cd
L3n1ad <danieltoth0910@gmail.com>
2020-06-09 14:56:02 +0100

    add inputChange and onFormSubmit functions

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchBar = () => {
+  const [term, setTerm] = useState("");
+
+  function inputChange(e) {
+    setTerm(e.target.value);
+  }
+
+  function onFormSubmit(e) {
+    e.preventDefault();
+    // TODO: make sure we call callback from parent component
+  }
   return (
     <div className="search-bar ui segment">
-      <form className="ui form">
+      <form className="ui form" onSubmit={(e) => onFormSubmit(e)}>
         <div className="field">
           <label>Video Search</label>
-          <input type="text"></input>
+          <input type="text" value={term} onChange={(e) => inputChange(e)} />
         </div>
       </form>
     </div>
