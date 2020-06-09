ef04cfedeb9fb550aede10ef4016be57f6bf0ff9
L3n1ad <danieltoth0910@gmail.com>
2020-06-09 16:16:17 +0100

    add axios config

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,12 +1,5 @@
 import axios from "axios";
 
-const KEY = process.env.REACT_APP_YOUTUBE_KEY;
-
 export default axios.create({
-  baseUrl: "https://www.googleapis.com/youtube/v3",
-  params: {
-    part: "snippet",
-    maxResults: 5,
-    key: `${KEY}`,
-  },
+  baseURL: "https://www.googleapis.com/youtube/v3",
 });
